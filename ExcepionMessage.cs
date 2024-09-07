7d326ad0b57212c5d53631d7190bf834d0ea7d43
Tiktak133 <tiktakowski133@gmail.com>
2024-09-07 22:48:04 +0200

    Create ExcepionMessage.cs

---
+++ ExcepionMessage.cs
@@ -0,0 +1,50 @@
+//20.11.2023
+"No może, może",
+"Ich habe Nylium gegesen - es war lecker",
+"Życie jest zbyt krótkie aby bać się nowych rzeczy",
+"All truly strong people are kind",
+"Śmieszny żart na dziś - Co mówi Informatyk na ślubie? - \"Pobieranie zakończone\" haahahahhaahpomóżmihaha", //nie śmieszne i mówie to ja z września 2024
+//
+"Kiedy postanowisz zmienić coś w swoim życiu? Nie no, poważnie pytam",
+"Dlaczego ludzie przejmują się czyjąś opinią, nawet gdy nie ma ona odniesienia w rzeczywistości?",
+"Nie zastanawiaj się nad swoimi błędami tylko rób to co sprawia ci przyjemność - nie ma czasu do stracenia",
+"Nie bój się mówić o swoich pomysłów i pragnieniach, nawet jeżeli możliwe jest to, że spotkasz sie z krytyką innych. Znajdź ludzi którzy będą cię wspierać w twoich zainteresowaniach i pasjach",
+"Jeżeli ktoś spyta \"Na co ci to?\" Nie ma sensownej odpowiedzi. Wszystko można powiązać z sensem życia ludzkiego lub jego brakiem, więc nie zastanawiaj się więcej i idź dalej - dokonaj czegoś wielkiego",
+"Jeżeli zacznie ci odkurwiać i rozpoczniesz pisanie tekstów motywacyjnych to wiedz, że nie ma dla ciebie ratunku - najprawdopodobniej c:",
+"Niektórzy mówią, że nie można pić jak się programuje, ale to nie prawda. Można tylko trzeba wstawać rano, na tym polega odpowiedzialność - ps. najpierw to trzeba znaleźć kursor na ekranie, nie polecam",
+//"Zajmuj się tym co umocni cię w byciu prawdziwym człowiekiem (zgódźmy się że każdy usłyszy w tym zdaniu to co faktycznie chciał usłyszeć - jeżeli nie rozumiesz przekazu, oznacza to że nigdy nie byłeś w słabym stanie psychicznym)",
+//"Jeżeli miałeś kiedyś chęć ukrycia różnych notek na wypadek śmierci mających na celu wzbudzić u czytelnika wyrazy współczycia, to radzę poszukać pomocy u kogoś bliskiego albo specjalisty",
+//09.05.2024
+"Życie ma sens tylko wtedy gdy masz kogoś z kim możesz się nim dzielić, swoimi chwilami, przemyśleniami",
+//11.05.2024
+"Zamknięcie się na ludzi nie pomaga w skupieniu",
+//12.05.2024
+"Jak pomóc komuś kto twojej pomocy nie chce? Chcesz to naprawić ale tylko pogarszasz. Nic nie mówisz. Zasypiasz z bulem, żeby nastepnego dnia powtórzyć błędy",
+//26.05.2024
+"Nie odzywasz się słowem, żeby dać się wykazać i nie pogorszać sytuacji. W zamian zostajesz nazwany ignorantem i nikim ważnym - Witamy w społeczeństwie",
+"Nie wiem jak długo będe miał jeszcze siłe starać się o wybaczenie. Mam dość wszystkiego, a tak przy okazji to ci się program wysypał. em jej?",
+"Zostałem wyzwany za to, że piłem alkohol trzeci raz w miesiącu, nie wiem dlaczego. Ale jeżeli faktycznie przesadzam to mam nadzieje, że za czwartym razem nie będe się musiał bucić w tym świecie. Na czym my to? A no, program nie działa. Daj sobie spokój, nie naprawie go nigdy nie mam siły",
+//01.06.2024
+"BŁAD - Podoba mi się jak zbiór tekstów z błędami został zamieniany na liste moich zarzaleń i przemyśleń życiowych",
+"Może i jestem teraz wrakiem człowieka ale za to nie kryje się z tym co siedzi mi w głowie z nikim, no to może nie jest tak dobre | Nylium się wysypało",
+"Uwielbiam jak łazisz za kimś cały dzień licząc na to ze ten powie cokolwiek do ciebie chociaż raz, ALE nie bo po chuj. Czekasz na gościa żeby nie musiał iść sam, a ten cie mija i idzie (wręcz biegnie) do innej osoby zostawiająć cie 15 metrów za sobą - i tak cały dzień. Czy to nie smutne?",
+"Czy można wierzyć komuś kto ciągle przysięga, że się z tobą spotka? Nie sądze, tak więc ten program też nie będzie działać nigdy poprawnie dopuki się nic nie zmieni",
+"Dlaczego widzisz ten komunikat? - Żeby wiedzieć, że mam chujowe samopoczucie i nie mam ochoty naprawić błedu programu, nie mam komu się wyżalić, a żeby nie zwariować pisze takiego gówno - Nylium 2024",
+"Jeżeli on napisze coś do mnie sam, bez mojej wcześniejszej interwencji to dosłownie naprawie ten zasrany program - NIE MOŻLIWE", //sam kurwa powiedziałeś że masz wyjebane na ludzi i nie zależy ci na znajomości z innymi, dobrze to pamiętam.  Więc nic dziwnego że nie zależy ci na znajomości ze mną
+//28.06.2024
+"Dzięki ig",
+//30.06.2024
+"The most annoying thing about idiocy is that you can't explain it to an idiot.", //Lubie to brzmi jak ja, bardzo NIE POCIESZAJĄCE
+//06.07.2024
+"Nigdy nie planuj z nikim niczego do przodu. Planuj swoją własną przyszłość, to jedyne nad czym masz kontrole",
+//05.08.2024
+"Kiedy jesteś 500 km od domu i wiesz, że choćby się cud stał nie przyspieszyć prac nad projektem ale mimo to podejmujesz próbe jego dokończenia. To nie ważne ile zrobisz, zawsze będzie to posuwanie się do przodu",
+"Czyli tak, nie ma sensu planować niczego do przodu. Człowiek z takim podejściem żyć będzie bez celu i stresu, ale jaki to ma sens? Czy będzie szczęśliwy?",
+//08.08.2024
+"Nigdy nie ufaj gównie od mety XD (facebooka)", //Telefon mi sam jakieś gówno zaczął wysyłać jak chodziłem po górach XD
+//07.09.2024
+"... - jedyny znak który nie mówi nic a jednak daje spokój", //Chodzi o to, że lepsza odpowiedz która nic nie wnosi, ale jest i daje spokój, niż jej brak
+"Dobrze wiem gdy zrobie coś nie tak, zawsze wtedy dostane opierdol z GŁĘBOKIM wyjaśnieniem jakim zjebem jestem. Aż dziwne, że jedna osoba woli odejść w cień i zamknąć sie w sobie. Bardzo rozważne. Szkoda, że wtedy nie wiesz co zrobiłeś nie tak",
+"Milczenie jest złotem, podziwiam jak możesz być tak spokojny gdy się na ciebie wydzieram, to bardzo dojrzałe - mówił mi ojciec. Szkoda, że nie potrafie czasem zamknąc ryja na zawołanie",
+"Jak żyć?",
+"Ludzie nie boją się upadków czy porażek. Boją się szczęścia, bo tak szybko jak sie pojawia, znika nieodwracalnie", //i tu w cale nie chodzi o mnie
